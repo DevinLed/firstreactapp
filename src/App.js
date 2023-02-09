12686d17c3717d8af10b0995a84788d7969d589d
Devin Ledwell <devinledwell@gmail.com>
2023-02-09 19:04:13 -0330

    main

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <h1>This is my first react test on VS code</h1>
+            Testing out the functionality behind React for future projects
+          </p>
+        </header>
     </div>
   );
 }

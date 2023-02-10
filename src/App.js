fdc9b9c2940e0f1c4c88f232c5bcb7f4c64b4945
Devin Ledwell <devinledwell@gmail.com>
2023-02-09 20:54:08 -0330

    To do list tutorial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos( prevTodos => [...prevTodos, ...storedTodos] );
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos =>{
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+    }
+    function handleClearTodos(){
+      const newTodos = todos.filter(todo => !todo.complete)
+      setTodos(newTodos)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <h1>This is my first react test on VS code</h1>
-            Testing out the functionality behind React for future projects
-          </p>
-        </header>
-    </div>
-  );
+    <>
+    <TodoList todos={todos} toggleTodo={toggleTodo} />
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo}>Add Todo</button>
+    <button onClick={(handleClearTodos)}>Clear Completed Todos</button>
+    <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
+  )
 }
 
 export default App;
